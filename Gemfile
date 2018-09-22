d7818fe4747279097bc33bb5c94bc01456b00412
Nate Beaty <nate@clixel.com>
2018-09-22 13:32:49 -0500

    add output dir, add rudimentary auth, add illustrations, product_form_detail, audience_range

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
-# gem 'onix', path: '/Users/natebeaty/Sites/onix-ruby-nb/'
 gem 'onix', github: 'natebeaty/onix', branch: 'master'
 gem 'dotenv'
--- api.rb
+++ api.rb
@@ -1,4 +1,4 @@
-#require 'dotenv/load'
+require 'dotenv/load'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
@@ -7,15 +7,29 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 
 get '/' do
-  ONIX::Normaliser.process('sample.xml', 'newfile.xml')
-  # reader = ONIX::Reader.new(File.join(File.dirname(__FILE__),"sample.xml"), ::ONIX::APAProduct)
+  status 403
+  'Bad request.'
 end
 
 post '/api' do
-  json = JSON.parse(request.body.read)
-  # puts "I got some JSON: #{json['inspect']}"
-  xml_out_filename = 'output3.xml'
+  # rudimentary auth
+  api_key = params.fetch :api_key, ''
+  if api_key != ENV['ONIX_API_KEY']
+    status 403
+    return "Invalid api key."
+  end
 
+  # parse json input
+  json = JSON.parse(request.body.read)
+
+  # make sure we have some products
+  if !json['onixData'].key?('products') or json['onixData']['products'].empty?
+    status 403
+    return 'Bad request.'
+  end
+  xml_out_filename = "output/onix-output-%s.xml" % json['onixData']['products'][0][0]['productId']
+
+  # output xml file
   File.open(xml_out_filename, 'w') do |output|
     header = ONIX::Header.new
     header.from_company = json['onixData']['from']['company']
@@ -25,11 +39,7 @@ post '/api' do
 
     writer = ONIX::Writer.new(output, header)
 
-    # product = ONIX::Product.new
-
     json['onixData']['products'].each do |json_product|
-      # content_type :json
-      # return { :onix => json_product[0], :key2 => 'value2' }.to_json
       product = ONIX::APAProduct.new
 
       product.notification_type = 3
@@ -38,24 +48,26 @@ post '/api' do
       product.width = json_product[0]['width']
       product.height = json_product[0]['height']
       product.thickness = json_product[0]['thickness']
-      product.number_of_pages = json_product[0]['page_count'].to_i
+      product.number_of_pages = json_product[0]['pageCount'].to_i
 
       # images
       product.thumbnail_url = json_product[0]['thumbnail']
-      product.cover_url = json_product[0]['cover_url']
-      product.cover_url_hq = json_product[0]['cover_url_hq']
+      product.cover_url = json_product[0]['coverUrl']
+      product.cover_url_hq = json_product[0]['coverUrlHq']
 
       # ? what is this
-      product.proprietary_id = json_product[0]['product_id']
+      product.proprietary_id = json_product[0]['productId']
       # also unsure of this
-      product.record_reference = json_product[0]['product_id']
+      product.record_reference = json_product[0]['productId']
       # do we need both isbn?
       product.isbn10 = json_product[0]['isbn10']
       product.isbn13 = json_product[0]['isbn13']
 
       product.title = json_product[0]['title']
       product.subtitle = json_product[0]['subtitle']
-      product.add_contributor(json_product[0]['author_reverse'])
+      product.main_description = json_product[0]['description']
+      product.short_description = json_product[0]['shortDescription']
+      product.add_contributor(json_product[0]['authorReverse'])
 
       # product.imprint = "Malhame"
       product.publisher = json_product[0]['publisher']
@@ -70,47 +82,40 @@ post '/api' do
       product.supplier_email = json_product[0]['supplier']['email']
       product.supply_country = "US"
 
-      # product.bic_subjects =
       # subjects
-      json_product[0]['bisac_codes'].each do |bisac_code|
-        # product.bic_main_subject = bisac_code
+      json_product[0]['bisacCodes'].each_with_index do |bisac_code, i|
+        if i == 0
+          product.bic_main_subject = bisac_code
+        end
         product.add_bisac_subject(bisac_code)
       end
 
       # audience_range
-      # json_product[0]['audience_range']
-
-
-      # json_product[0]['description']
-      # json_product[0]['productForm']
-      # json_product[0]['productFormDetail']
-
+      product.audience_range = json_product[0]['audienceRange']
 
       # 10  Not yet available (needs expectedshipdate)
       # 11  Awaiting stock (needs expectedshipdate)
       # 20  Available
       # 21  In stock
       product.product_availability = 20
-      product.product_form = json_product[0]['product_form']
 
-      # product.illustration_type = json_product[0]['illustrationOtherContentType']
+      product.product_form = json_product[0]['productForm']
+      product.product_form_detail = json_product[0]['productFormDetail']
 
-      # product.product_form_detail = json_product[0]['product_form_detail']
+      product.add_illustration(json_product[0]['illustrationOtherContentType'])
 
+      #product.on_order = 20
       product.on_hand = json_product[0]['quantity']
       product.rrp_exc_sales_tax = json_product[0]['retail'].to_d
-      #product.on_order = 20
 
       writer << product
     end
     writer.end_document
   end
 
+  # now just return an xml file, PHP will send it along as a download
   content_type 'text/xml'
   File.read(xml_out_filename)
-  # response.headers['content_type'] = "application/octet-stream"
-  # attachment(project.name+'.tga')
-  # response.write(project.image)
 
   # content_type :json
   # {
