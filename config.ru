c85b8092905cfa5b7f2a1bc619c1bc046c70b2d2
Nate Beaty <nate@clixel.com>
2018-09-22 16:47:03 -0500

    basic fabfile, config.ru, ignore pyc, add env example

---
+++ config.ru
@@ -0,0 +1,5 @@
+require 'rubygems'
+Gem.clear_paths
+
+require "./api"
+run OnixApi
---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import *
+
+env.hosts = ['microcosmpublishing.com']
+env.user = 'microcosmpub'
+env.project_name = 'onix-sinatra'
+env.path = '/home/microcosmpub/webapps/passenger/%s' % env.project_name
+env.git_branch = 'master'
+
+def deploy():
+  update()
+  bundle()
+  restart()
+
+def update():
+  with cd(env.path):
+    run('git pull origin %s' % env.git_branch)
+
+def bundle():
+  with cd(env.path):
+    run('bundle install --quiet')
+
+def restart():
+  with cd(env.path):
+    run('../bin/restart')
